18caa947dfc606d5e00ed7bb330f2a8aea4581e5
codeclub <codeclub@smms-rm23-33.smusd.local>
2017-12-06 14:58:21 -0800

    spiral

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,21 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot cheezit = new Robot();
+	cheezit.setSpeed(100);
+	cheezit.move(250);	
+	cheezit.turn(-180);
+	cheezit.penDown();
+	cheezit.setPenWidth(10);
+	cheezit.setPenColor(Color.cyan);
+	cheezit.sparkle();
+	cheezit.miniaturize();
 	
+	for(int i = 0; i <4; i++) {
 		
+	
+
 		
 	}
+}
 }
